fa8c7e30663846ce1c31ccf1c2933ee6b891e89b
solens <s.solen@hotmail.com>
2015-10-11 11:08:21 +0100

    All figures

---
+++ plot1.R
@@ -0,0 +1,17 @@
+full_data<-read.table("household_power_consumption.txt",sep = ";",header=TRUE,na.strings = "?")
+
+Date<-as.POSIXct(full_data$Date,format="%d/%m/%Y")
+Date_Time<-paste(Date,full_data$Time,sep = " ")
+Date_Time<-strptime(Date_Time,format="%Y-%m-%d %H:%M:%S")
+
+full_data2<-cbind(Date,Date_Time,full_data[,-c(1,2)])
+
+start_date<-as.POSIXct("2007-02-01",tz="GMT")
+end_date<-as.POSIXct("2007-02-03",tz="GMT")
+
+data<-subset(full_data2,full_data2$Date_Time>=start_date)
+data<-subset(data,data$Date_Time<end_date)
+
+png(file="plot1.png")
+hist(data$Global_active_power,main="Global Active Power",xlab = "Global Active Power (kilowatts)",ylab="Frequency",col="red")
+dev.off()
---
+++ plot4.R
@@ -0,0 +1,29 @@
+full_data<-read.table("household_power_consumption.txt",sep = ";",header=TRUE,na.strings = "?")
+
+Date<-as.POSIXct(full_data$Date,format="%d/%m/%Y")
+Date_Time<-paste(Date,full_data$Time,sep = " ")
+Date_Time<-strptime(Date_Time,format="%Y-%m-%d %H:%M:%S")
+
+full_data2<-cbind(Date,Date_Time,full_data[,-c(1,2)])
+
+start_date<-as.POSIXct("2007-02-01",tz="GMT")
+end_date<-as.POSIXct("2007-02-03",tz="GMT")
+
+data<-subset(full_data2,full_data2$Date_Time>=start_date)
+data<-subset(data,data$Date_Time<end_date)
+
+png(file="plot4.png")
+par(mfcol=c(2,2))
+
+plot(data$Date_Time,data$Global_active_power,type = "l",col="black",xlab="",ylab="Global Active Power",fg="black")
+
+plot(data$Date_Time,data$Sub_metering_1,type = "l",col="black",xlab="",ylab="Energy sub metering")
+lines(data$Date_Time,data$Sub_metering_2,type = "l",col="red")
+lines(data$Date_Time,data$Sub_metering_3,type = "l",col="blue")
+legend("topright",pch = "_",bty="n",col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+
+plot(data$Date_Time,data$Voltage,type = "l",col="black",xlab="Date and Time",ylab="Voltage",fg="black")
+
+plot(data$Date_Time,data$Global_reactive_power,type = "l",col="black",xlab="Date and Time",ylab="Global Reactive Power",fg="black")
+
+dev.off()
